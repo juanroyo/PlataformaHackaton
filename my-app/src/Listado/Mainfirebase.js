6c55d9ce53f7908aa6ee9da7c1f7ae3d27c97207
unknown <carrascosajose@hotmail.com>
2020-02-11 12:38:35 +0100

    Added firebase to the project

---
+++ my-app/src/Listado/Mainfirebase.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+
+
+  <script src="https://www.gstatic.com/firebasejs/7.8.1/firebase-app.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/7.8.1/firebase-database.js"></script>
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDPEx_ZRZ6eAW-JdcDB5tdfedrN5slTJoI",
+    authDomain: "plataformahack.firebaseapp.com",
+    databaseURL: "https://plataformahack.firebaseio.com",
+    projectId: "plataformahack",
+    storageBucket: "plataformahack.appspot.com",
+    messagingSenderId: "908674634993",
+    appId: "1:908674634993:web:1079035bf0996603964550"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
